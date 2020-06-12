c0cda115937a12ebb37e2e93d9f2b3cdc7f2ed0f
Fuji Pebri <pebri86@gmail.com>
2020-06-12 13:05:59 +0700

    merge upstream
    Signed-off-by: Fuji Pebri <pebri86@gmail.com>

--- wasp/apps/settings.py
+++ wasp/apps/settings.py
@@ -13,7 +13,7 @@ class SettingsApp():
     ICON = icons.settings
 
     def __init__(self):
-        self.pages = ('Brightness', 'Sleep Timer')
+        self.pages = ('Brightness', 'Sleep Timer', 'Wake Lift')
         self.page = self.pages[0]
         self._scroll = wasp.widgets.ScrollIndicator()
         self._slider = wasp.widgets.Slider(3, 10, 90)
@@ -25,12 +25,18 @@ class SettingsApp():
                                   wasp.EventMask.SWIPE_UPDOWN)
 
     def touch(self, event):
-        if self.page == 'Brightness':
+        if self.page == self.pages[0]:
             self._slider.touch(event)
             wasp.system.brightness = self._slider.value + 1
-        elif self.page == 'Sleep Timer':
+        elif self.page == self.pages[1]:
             self._slider2.touch(event)
             wasp.system.blank_after = (self._slider2.value + 1) * 5
+        elif self.page == self.pages[2]:
+            if wasp.system.wake_lift:
+                wasp.system.wake_lift = False
+            else:
+                wasp.system.wake_lift = True
+            
         self._update()
 
     def swipe(self, event):
@@ -51,16 +57,19 @@ class SettingsApp():
     def _draw(self):
         """Redraw the display from scratch."""
         wasp.watch.drawable.fill()
-        if self.page == 'Brightness':
+        if self.page == self.pages[0]:
             wasp.watch.drawable.string('Brightness', 0, 6, width=240)
             self._slider.draw()
-        elif self.page == 'Sleep Timer':
+        elif self.page == self.pages[1]:
             wasp.watch.drawable.string('Sleep Timer', 0, 6, width=240)
             self._slider2.draw()
+        elif self.page == self.pages[2]:
+            wasp.watch.drawable.string('Wake by Lift', 0, 6, width=240)           
+
         self._update()
 
     def _update(self):
-        if self.page == 'Brightness':
+        if self.page == self.pages[0]:
             if wasp.system.brightness == 3:
                 say = "High"
             elif wasp.system.brightness == 2:
@@ -69,7 +78,7 @@ class SettingsApp():
                 say = "Low"            
             self._slider.update()
             wasp.watch.drawable.string(say, 0, 150, width=240)
-        elif self.page == 'Sleep Timer':
+        elif self.page == self.pages[1]:
             if wasp.system.blank_after == 5:
                 say = "5s"
             elif wasp.system.blank_after == 10:
@@ -77,5 +86,11 @@ class SettingsApp():
             else:
                 say = "15s"
             self._slider2.update()
-            wasp.watch.drawable.string(say, 0, 150, width=240)
+            wasp.watch.drawable.string(say, 0, 150, width=240)        
+        elif self.page == self.pages[2]:
+            if wasp.system.wake_lift:
+                wasp.watch.drawable.string("Enabled", 0, 150, width=240)
+            else:
+                wasp.watch.drawable.string("Disabled", 0, 150, width=240)
+
         self._scroll.draw()
